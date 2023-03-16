3462c9901a7efb6a54fe64382a7ebd759dae1845
Nandini-Kaushik09 <123489007+Nandini-Kaushik09@users.noreply.github.com>
2023-03-16 12:43:37 +0530

    Add files via upload

---
+++ scm file/scm.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int i,n;
+    printf("enter value of n");
+    scanf("%d",&n);
+    i=1;
+    while (i<=n){
+        printf("%d",i);
+        i++;
+    }
+}    
